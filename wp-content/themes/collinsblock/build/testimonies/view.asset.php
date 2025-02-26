f258ebb37a2df4f33a70d1d230e4fe687dce7c75
Ty Streets <tstreets@iu.edu>
2025-02-26 07:02:05 -0500

    Add code

---
+++ wp-content/themes/collinsblock/build/testimonies/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1f2b2315d69d20336413');
---
+++ wp-content/themes/collinsblock/build/testimony-gallery/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5d3174ac2821c4611023');
